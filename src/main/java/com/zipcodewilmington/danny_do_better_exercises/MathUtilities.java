7362c7ee79ae585863f2712696a5b46174d58f89
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-01 21:59:47 -0400

    done long and short multiplication

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/MathUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/MathUtilities.java
@@ -184,7 +184,7 @@ public class MathUtilities {
      * @return product of `multiplicand` by `multiplier`
      */
     public Long multiply(long multiplicand, long multiplier) {
-        return null;
+        return Math.multiplyExact(multiplicand, multiplier);
     }
 
     /**
@@ -193,7 +193,7 @@ public class MathUtilities {
      * @return product of `multiplicand` by `multiplier`
      */
     public Short multiply(short multiplicand, short multiplier) {
-        return null;
+        return (short) (multiplicand*multiplier);
     }
     /**
      * @param multiplicand value to be multiplied
