8885b932f51e21c4bc29c5984e0fef9cb28fcb02
Imir <imir@Zipcoders-MacBook-Pro-4.local>
2023-11-01 22:53:49 -0400

    done second word

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
@@ -80,7 +80,8 @@ public class StringUtilities {
      * @return the second word of a string delimited by spaces.
      */
     public static String getSecondWord(String spaceDelimitedString){
-        return null;
+        String[] splitted = spaceDelimitedString.split(" ");
+        return splitted[1];
     }
 
     /**
